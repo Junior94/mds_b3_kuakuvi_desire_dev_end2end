4b79d3266eaadb16fc7c6d3e547d004c6bdd2cce
Junior <kuakuvidesire@gmail.com>
2021-01-24 23:11:41 +0100

    Check block login:header wrapper

--- tests/uber.js
+++ tests/uber.js
@@ -9,3 +9,13 @@ describe('Uber', function() {
       .assert.elementPresent('head')
       .end();
   });
+
+test('Check block login in the header wrapper', function (browser) {
+    browser
+      .waitForElementVisible('body')
+      .useXpath()
+      .assert.visible('//[@id="main"]/nav/div/ul[4]/li[4]/button', 'Sign in button is visible')
+      .assert.visible('//[@id="main"]/nav/div/ul[4]/li[5]/button', 'Sign up button is visible')
+      .useCss()
+      .end();
+  });
