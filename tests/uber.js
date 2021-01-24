e444ede53a297e637f609449e143438d1d1f6126
Junior <kuakuvidesire@gmail.com>
2021-01-24 23:09:10 +0100

    Uber:Open Uber Website

--- tests/uber.js
+++ tests/uber.js
@@ -1,27 +1,11 @@
-describe('Ecosia example', function() {
+describe('Uber', function() {
 
-    beforeEach(browser => browser.url('https://www.ecosia.org/'));
+  beforeEach(browser => browser.url('https://www.uber.com/fr/fr/%27));
 
-    test('Demo test ecosia.org', function (browser) {
-      browser
-        .waitForElementVisible('body')
-        .assert.titleContains('Ecosia')
-        .assert.visible('input[type=search]')
-        .setValue('input[type=search]', 'nightwatch')
-        .assert.visible('button[type=submit]')
-        .click('button[type=submit]')
-        .assert.containsText('.mainline-results', 'Nightwatch.js')
-        .end();
-    });
-
-/*
-// Exemple pour basculer sur du XPATH
-.useXpath() 
-   .waitForElementPresent('//button[@id="/sign-in/"]')
-   .waitForElementPresent('//button[@id="/signup/"]')
-// Revenir sur du CSS
-.useCSS()
-*/
-
-
-});
+  test('Open Uber Website', function (browser) {
+    browser
+      .waitForElementVisible('body')
+      .assert.elementPresent('title')
+      .assert.elementPresent('head')
+      .end();
+  });
