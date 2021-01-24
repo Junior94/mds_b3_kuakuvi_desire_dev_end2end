de291a3614734325ff5c52580ee6385c970a6f89
Junior <kuakuvidesire@gmail.com>
2021-01-24 23:13:29 +0100

    CHeck login block in two sections

--- tests/uber.js
+++ tests/uber.js
@@ -19,3 +19,15 @@ test('Check block login in the header wrapper', function (browser) {
       .useCss()
       .end();
   });
+
+  test('Check login block in two sections', function (browser) {
+    browser
+      .waitForElementVisible('body')
+      .useXpath()
+      .assert.visible('//[@id="main"]/nav/div/ul[4]/li[4]/button', 'Sign in button is visible')
+      .click('//[@id="main"]/nav/div/ul[4]/li[4]/button', () => console.log('Click on sign in button'))
+      .assert.visible('//[@id="root"]/div/div/div[2]/div/div[3]/div/div[2]/div/div[4]/section/div/div/div/div/div[1]/a', 'Driver sign in button is visible')
+      .assert.visible('//[@id="root"]/div/div/div[2]/div/div[3]/div/div[2]/div/div[4]/section/div/div/div/div/div[3]/a', 'Passenger sign in button is visible')
+      .useCss()
+      .end();
+  });
